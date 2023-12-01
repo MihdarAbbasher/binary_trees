aeca5aaab1c70bc892f515f33e1e6ecbd0275354
MihdarAbbasher <mihdar.ph@gmail.com>
2023-12-01 02:13:37 +0200

    t101style

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -28,7 +28,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
  * @func: pointer to a  node
  * Return: void
  */
- 
+
 void print_level_nodes(const binary_tree_t *tree, int level, void (*func)(int))
 {
 	if (!tree)
@@ -43,7 +43,8 @@ void print_level_nodes(const binary_tree_t *tree, int level, void (*func)(int))
 }
 
 /**
- * binary_tree_levelorder - goes through a binary tree using level-order traversal
+ * binary_tree_levelorder - goes through a binary tree
+ * using level-order traversal
  * @tree: pointer to the root node of the tree to traverse
  * @func: pointer to a function to call for each node
  * Return: void
